9e7234e58aa65428ef0d27a5abcfeb35ca782920
czekla <czekla@gmail.com>
2015-04-13 20:37:24 +0200

    Csibe

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="css/jquery-ui.css"/>
+        <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+        <script src="js/jquery-ui.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+    </head>
+    <body>
+        <?php
+        echo "hello";
+        ?>
+    </body>
+</html>
